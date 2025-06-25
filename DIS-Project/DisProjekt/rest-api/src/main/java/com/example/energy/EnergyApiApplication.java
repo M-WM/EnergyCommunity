d7329bee6d8cb0fb9364a048b42f063db488068f
M-WM <wi23b172@technikum-wien.at>
2025-06-25 21:55:03 +0200

    Add files via upload
    JavaFxGui changes upload

---
+++ DIS-Project/DisProjekt/rest-api/src/main/java/com/example/energy/EnergyApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyApiApplication.class, args);
+    }
+}
